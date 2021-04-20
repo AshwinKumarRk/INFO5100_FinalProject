94d6319b345f8deeb542669892899df93b4df28d
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-04-20 18:31:55 -0400

    Implement GroceryStoreDirectory

--- FairFood/src/Business/GroceryStore/GroceryStoreDirectory.java
+++ FairFood/src/Business/GroceryStore/GroceryStoreDirectory.java
@@ -5,10 +5,32 @@
  */
 package Business.GroceryStore;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ashwin
  */
 public class GroceryStoreDirectory {
+    private ArrayList<GroceryStore> grocerystoreList;
     
+    public GroceryStoreDirectory() {
+        grocerystoreList = new ArrayList();
+    }
+    
+    public ArrayList<GroceryStore> getStoreList() {
+        return grocerystoreList;
+    }
+    
+    public GroceryStore createItem(GroceryStore grocerystore){
+        grocerystoreList.add(grocerystore);
+        return grocerystore;
+    }
+    
+    public GroceryStore updateItem(GroceryStore grocerystore,String productName,String productType){
+        grocerystore.setItemName(productName);
+        grocerystore.setItemType(productType);
+
+        return grocerystore;
+    }
 }
