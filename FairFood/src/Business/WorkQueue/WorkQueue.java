d7f267c8a5f8cb929b83b6a8cbce84ac3a7e6464
Trupti Rajendra Raut <raut.t@northeastern.edu>
2021-04-21 11:16:21 -0400

    WorkReq and WorkQue

--- FairFood/src/Business/WorkQueue/WorkQueue.java
+++ FairFood/src/Business/WorkQueue/WorkQueue.java
@@ -5,10 +5,21 @@
  */
 package Business.WorkQueue;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author truptiraut
  */
 public class WorkQueue {
+     private ArrayList<WorkRequest> workRequestList;
+
+    public WorkQueue() {
+        workRequestList = new ArrayList();
+    }
+
+    public ArrayList<WorkRequest> getWorkRequestList() {
+        return workRequestList;
+    }
     
 }
