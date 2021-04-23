f77984ec7d23fa26c7117eea3dd3f059eafe2cb1
Trupti Rajendra Raut <raut.t@northeastern.edu>
2021-04-23 00:25:50 -0400

    Admin Roles-1

--- FairFood/src/Business/Role/Role.java
+++ FairFood/src/Business/Role/Role.java
@@ -1,6 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package Business.Role;
@@ -8,7 +7,7 @@ package Business.Role;
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
-import Business.Organisation.Organisation;
+import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
 
@@ -18,47 +17,47 @@ import javax.swing.JPanel;
  */
 public abstract class Role {
     
-     
     public enum RoleType{
-        Admin("Admin Role"),
-        ClientServiceAssociate("Client Service Associate Role"),
-        ClientServiceManager("Client Service Manager Role"),
-        InventoryAssistant("Inventory Assistant Role"),
-        InventoryManager("Inventory Manager Role"),
-        Donor("Donor Role");
-        
-        
-        private String value;
-        private RoleType(String value)
-        {
+        Admin("Admin"),
+        Receiver("Receiver"),
+        QualityAssurance("QualityAssurance"), 
+        QualityAssuranceAdmin("QualityAssurance Admin"),
+        FoodSafetyAdmin("FoodSafety Admin"),
+        FoodAdvisorAdmin("FoodAdvisor Admin"),
+        Volunteer("Volunteer"),
+        EventManager("EventManager"),
+         GroceryStoreManager("GroceryStore Manager"),
+        DonationManager("DonationManager"),
+        DeliveryMan("Delivery Man");
+      
+       
+       private String value;
+       
+        private RoleType(String value){
             this.value = value;
         }
 
         public String getValue() {
             return value;
         }
-        
+
         @Override
-        public String toString()
-        {
-            return this.value;
+        public String toString() {
+            return value;
         }
     }
-   public abstract JPanel createWorkArea(JPanel userProcessContainer, 
+    
+    public abstract JPanel createWorkArea(JPanel userProcessContainer,  
             Enterprise enterprise, 
-            Organisation organisation,
+            Organization organization,
             UserAccount account, 
             EcoSystem business,
             Network network);
-
 
     @Override
     public String toString() {
         return this.getClass().getName();
     }
-
-}
-
     
-
-
+    
+}
--- FairFood/src/Business/Role/SystemAdminRole.java
+++ FairFood/src/Business/Role/SystemAdminRole.java
@@ -1,6 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package Business.Role;
@@ -8,21 +7,22 @@ package Business.Role;
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
-import Business.Organisation.Organisation;
+import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
+import userinterface.SystemAdminWorkArea.SystemAdminWorkAreaJPanel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author truptiraut
  */
-public class SystemAdminRole extends Role {
-    
-     @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organisation organisation, UserAccount account, EcoSystem business, Network network) {
+public class SystemAdminRole extends Role{
+
+   
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
         return new SystemAdminWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
     }
-
-
     
 }
