3faacd098a34e7e4d796bad8aa95a05e28679bbb
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-04-23 01:02:12 -0400

    Modify GroceryStoreManager Role

--- FairFood/src/Business/Role/GroceryStoreManager.java
+++ FairFood/src/Business/Role/GroceryStoreManager.java
@@ -1,6 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package Business.Role;
@@ -8,25 +7,21 @@ package Business.Role;
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
-import Business.Organization.GroceryStoreOrganisation;
-import Business.Organisation.Organisation;
+import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
-import UserInterface.DeliveryPerson.DeliveryManWorkAreaJPanel;
-import UserInterface.GroceryStore.GroceryStoreJPanel;
+import UserInterface.AdministrativeRole.AdminWorkAreaJPanel;
 import javax.swing.JPanel;
+import UserInterface.GroceryStoreManager.ManageGroceryStoreJPanel;
 
 /**
  *
  * @author ashwin
  */
 public class GroceryStoreManager extends Role{
+
     @Override
-    public String toString()
-    {
-        return "Donor";
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        return new ManageGroceryStoreJPanel(userProcessContainer, enterprise, organization, account, business, network);
     }
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organisation organisation, Enterprise enterprise, EcoSystem system, ProductInfoList productInfolist, FoodStock foodstock) {
-        return new GroceryStoreJPanel(userProcessContainer, account, (GroceryStoreOrganisation) organisation, enterprise, system, productInfolist);
-    }
+ 
 }
