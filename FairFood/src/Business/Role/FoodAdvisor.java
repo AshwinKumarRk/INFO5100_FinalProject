9bf28e4c96f9513da7170af8777368aea721f48f
VishalNarasimhan <narasimhan.v@northeastern.edu>
2021-04-23 14:04:50 -0400

    Add FoodAdvisor Role

---
+++ FairFood/src/Business/Role/FoodAdvisor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.FoodAdvisorOrganisation;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import UserInterface.FoodSafetyOfficer.FoodSafetyOfficerWorkAreaJPanel;
+
+/**
+ *
+ * @author visha
+ */
+public class FoodAdvisor extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        return new FoodSafetyOfficerWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
+}
