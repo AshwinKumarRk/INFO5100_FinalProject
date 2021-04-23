ab0e25a820d0d0b89901201f8c834b040f1ba7d0
VishalNarasimhan <narasimhan.v@northeastern.edu>
2021-04-23 14:02:29 -0400

    Modify DonationManager Role

---
+++ FairFood/src/Business/Role/DonationManagerRole.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Organization.DonationOrganization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import UserInterface.EventManagerRole.EventWorkAreaJPanel;
+
+/**
+ *
+ * @author visha
+ */
+public class DonationManagerRole extends Role {
+    
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        
+        return new EventWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
+    
+}
